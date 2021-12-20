639beef130cd40a0bd83b03a698c2c0835deaba8
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-12-20 10:41:09 +0200

    adding div id in index.html file

--- src/Maze.js
+++ src/Maze.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-import { ReactDOM } from 'react-dom';
-import {Grid,Paper,Box, Button} from "@material-ui/core"
-import Cell from './Cell';
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, Grid, Box, Paper } from "@material-ui/core";
+import Cell from "./Cell";
+import Path from "./Path";
+import rat from "./assets/rat.png";
+import cheese from "./assets/cheese.jpg";
+import "./index.css";
 
-class Maze extends Component {
+class Maze extends React.Component {
     matrix = [2];
     paths = [];
     idx = 0;
@@ -17,7 +20,12 @@ class Maze extends Component {
     }
 
     findColor  = (path) => {
-
+        this.pathIdx += 1;
+        let result = path[this.pathIdx];
+        if (this.pathIdx === 14){
+            this.pathIdx = 0;
+        }
+        return result;
     }
 
     generateMatrix = () => {
@@ -34,14 +42,74 @@ class Maze extends Component {
 
     inMaze = (x,y,visited) => {
         //if cell is valid return true
+        return(
+            0<= x <4 && 0 <= y <4 && this.matrix[x*4 +y] > 0 && visited[x*4 +y] == 0
+        );
     }
 
     calculatePaths = () => {
-
+        let visited =[];
+        for (let i = 0; i<16; i++){
+            visited[i] = 0;
+        }
+        this.mazeUtil(visited, 0, 0, []);
+        console.log(this.numPaths);
+        console.log(this.paths);
+        return this.paths;
     };
 
     mazeUtil = (visited, x, y, currentPath) => {
+        if(x==3 && y==3){
+            this.numPaths += 1;
+            this.paths.push([...currentPath]);
+            visited[15] = 0;
+            return;
+        }
+        if(!this.inMaze(x,y,visited)) {
+            return;
+        }
+        visited[4*x +y] =1;
 
+        //move toward x direction
+        currentPath.push([x+1, y]);
+        this.mazeUtil(visited, x+1, y, currentPath);
+        currentPath.pop();
+
+        //move toward y direction
+        currentPath.push([x, y+1]);
+        this.mazeUtil(visited, x, y+1, currentPath);
+        currentPath.pop();
+
+        visited[4*x +y] = 0;
+        return;
+    };
+    getNumPaths = () => {
+        return (
+            <div style={{color: "white"}}>
+            <center>
+            <b>Total Paths = {this.numPaths} </b> {" "}
+            </center>
+            </div>
+        )
+    }
+
+    findPaths = () => {
+        const displayNumPaths = <this.getNumPaths></this.getNumPaths>;
+        ReactDOM.render(displayNumPaths, document.getElementById("count"));
+        let grids = document.getElementById("routes");
+
+        for (let i = 0; i < this.paths.length; i++){
+            const solution = (
+                <Path currentPath ={this.paths[i]} maze={this.matrix}></Path>
+            );
+            const id = Math.random();
+            const d = document.createElement("span");
+            d.id = id;
+            const space = document.createElement("br");
+            grids.appendChild(d);
+            grids.appendChild(space);
+            ReactDOM.render(solution, document.getElementById(id));
+        }
     }
     handleClick = (e) => {
         e.preventDefault();
@@ -52,69 +120,107 @@ class Maze extends Component {
         this.matrix=new Array();
         this.matrix = [2];
         this.generateMatrix();
-        
+        this.calculatePaths();
+
         return (
-            <div>
-                <center>
-                <h1>Rat in a maze</h1>
-                </center>
-                <Grid container spacing={2} justify="center" direction="column">
-                    <p>
-                        <Grid container spacing={1} justify="center" direction="row">
-                            <Grid item>
-                                <Paper elevation={3}>
-                                    <Box padding={2} height={50} width={50}>
-                                        <center></center>
-                                    </Box>
-                                </Paper>
-                            </Grid>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                        </Grid>
-                    </p>
+            <span className="maze">
+        <center>
+          <h1>Rat in a Maze</h1>
+        </center>
+        <br></br>
+        <Grid container spacing={2} justify="center" direction="column">
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Grid item>
+                <Paper elevation={3}>
+                  <Box padding={2} height={50} width={50}>
+                    <center>
+                      <img
+                        src={rat}
+                        height={75}
+                        width={75}
+                        vertical-align="middle"
+                      ></img>
+                    </center>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+            </Grid>
+          </p>
 
-                    <p>
-                        <Grid container spacing={1} justify="center" direction="row">
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                        </Grid>
-                    </p>
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+            </Grid>
+          </p>
 
-                    <p>
-                        <Grid container spacing={1} justify="center" direction="row">
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                        </Grid>
-                    </p>
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+            </Grid>
+          </p>
 
-                    <p>
-                        <Grid container spacing={1} justify="center" direction="row">
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Cell N={this.index()}></Cell>
-                            <Grid item>
-                                <Paper elevation={3}>
-                                    <Box padding={2} height={50} width={50}>
-                                        <center></center>
-                                    </Box>
-                                </Paper>
-                            </Grid>
-                            
-                        </Grid>
-                    </p>
-
-                </Grid>
-                <div>
-                    <center><Button onClick={this.handleClick} style ={{margin:15, color: "white", background:"#4CAF50", padding: 8}}>Generate new maze</Button></center>
-                </div>
-            </div>
-        );
-    }
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Cell N={this.index()}></Cell>
+              <Grid item>
+                <Paper elevation={3}>
+                  <Box padding={2} height={50} width={50}>
+                    <center>
+                      <img
+                        src={cheese}
+                        height={70}
+                        width={70}
+                        vertical-align="middle"
+                      ></img>
+                    </center>
+                  </Box>
+                </Paper>
+              </Grid>
+            </Grid>
+          </p>
+          <div>
+            <center>
+              <Button
+                onClick={this.handleClick}
+                style={{
+                  margin: 15,
+                  color: "white",
+                  background: "#4CAF50",
+                  padding: 8,
+                }}
+              >
+                <b>Generate new maze</b>
+              </Button>
+              <Button
+                onClick={this.findPaths}
+                style={{
+                  margin: 15,
+                  color: "white",
+                  background: "#4CAF50",
+                  padding: 8,
+                }}
+              >
+                <b> Find Paths </b>
+              </Button>
+              <br></br>
+            </center>
+          </div>
+        </Grid>
+      </span>
+    );
+  }
 }
 
 export default Maze;
--- src/Path.js
+++ src/Path.js
@@ -1,40 +1,130 @@
-import React from 'react';
-import {Grid,Paper,Box} from "@material-ui/core"
-import Cell from './Cell';
-import "./index.css"
+import React from "react";
+import { Grid, Paper, Box } from "@material-ui/core";
+import Cell from "./Cell";
+import rat from "./assets/rat.png";
+import cheese from "./assets/cheese.jpg";
+import "./index.css";
 
-import React, { Component } from 'react';
+class Path extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class Path extends Component {
-    constructor(props){
-        super(props)
+  pathIdx = 0;
+  foundPath = [];
+
+  createFoundPath = () => {
+    for (let i = 0; i < 16; i++) {
+      this.foundPath.push(0);
     }
 
-    pathIdx = 0;
-    foundPath =[];
-
-    createFoundPath = () => {
-        //loop for i matrix with value 16
-        // this.foundPath.push(0)
-
-        //loop for j matrix with value 16
-        //if this.props.maze[j] == 0
-        //this.foundPath[j] == 2
-
-        //loop to the currentPath for both x and y
-
-        //e.g
-        //for (let k= 0; k<this.props.currentPath.length; k++){
-            
-        }
+    for (let j = 0; j < 16; j++) {
+      if (this.props.maze[j] == 0) {
+        this.foundPath[j] = 2;
+      }
     }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+
+    for (let k = 0; k < this.props.currentPath.length; k++) {
+      let x = this.props.currentPath[k][0];
+      let y = this.props.currentPath[k][1];
+
+      this.foundPath[4 * x + y] = 1;
     }
+  };
+
+  findColor = () => {
+    this.pathIdx += 1;
+    let result = this.foundPath[this.pathIdx];
+    if (result == 2) {
+      result = 0;
+    } else if (result == 0) {
+      result = 2;
+    }
+
+    if (this.pathIdx == 14) {
+      this.pathIdx = 0;
+    }
+    return result;
+  };
+
+  printOnConsole = () => {
+    for (let i = 0; i < 16; i++) {
+      console.log(i + " " + this.props.currentPath[i]);
+    }
+  };
+
+  render() {
+    this.createFoundPath();
+    this.printOnConsole();
+    return (
+      <span className="path">
+        <br></br>
+        <Grid container spacing={2} justify="center" direction="column">
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Grid item>
+                <Paper style={{ background: "#4CAF50" }} elevation={3}>
+                  <Box padding={2} height={50} width={50}>
+                    <center>
+                      <img
+                        src={rat}
+                        height={75}
+                        width={75}
+                        vertical-align="middle"
+                      ></img>
+                    </center>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+            </Grid>
+          </p>
+
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+            </Grid>
+          </p>
+
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+            </Grid>
+          </p>
+
+          <p>
+            <Grid container spacing={1} justify="center" direction="row">
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Cell N={this.findColor()}></Cell>
+              <Grid item>
+                <Paper style={{ background: "#4CAF50" }} elevation={3}>
+                  <Box padding={2} height={50} width={50}>
+                    <center>
+                      <img
+                        src={cheese}
+                        height={70}
+                        width={70}
+                        vertical-align="middle"
+                      ></img>
+                    </center>
+                  </Box>
+                </Paper>
+              </Grid>
+            </Grid>
+          </p>
+        </Grid>
+      </span>
+    );
+  }
 }
 
 export default Path;
